e94e1b46c70b8b1b4a6dcb0a94af6b0156f67ca6
WillApollo <54325428+WillApollo@users.noreply.github.com>
2022-05-25 12:48:55 -0500

    Initializing Website
    The Rough Draft Version of my portfolio. Still have to add functionality to about me videos, portfolio section, add font, edit main banner image, and adjust margins.

---
+++ WillAndrewsPortfolioWebsite/index.js
@@ -0,0 +1,53 @@
+//Finding Oversized elements
+// $.each($('*'), function() { if ($(this).width() > $('body').width()) { console.log($(this).get(0)); } }).length;
+
+
+var affixTop = $('.aboutMeSection').offset().top;       // get initial position of the aboutMeSection 1058
+var unafix = 2000;
+$(window).scroll(function() {                  // assign scroll event listener
+
+
+    var currentScroll = $(document).scrollTop(); // get current position of Scroll
+
+
+    if (currentScroll >= affixTop - 3 && currentScroll <3500) {           // apply position: fixed if you pass the aboutMeSection
+        $('.profilePicture').css({                      // scroll to that element or below it
+            position: 'fixed',
+            top: '15%',
+            left: '7.25%',
+            width: '30%'
+        });
+        $('.navbar').css({
+           visibility: 'visible'
+        });
+    }
+else if (currentScroll >=3500 ){
+  $('.profilePicture').css({                      // if you scroll above it
+      position: 'absolute',
+      top: '155%',
+      left: '15%',
+      width: '60%'
+
+
+  });
+
+  $('.navbar').css({
+     visibility: 'visible'
+  });
+
+}
+    else {                                   // apply position: relative
+        $('.profilePicture').css({                      // if you scroll above it
+            position: 'absolute',
+            top: '15%',
+            left: '15%',
+            width: '60%'
+
+
+        });
+        $('.navbar').css({
+           visibility: 'hidden'
+        });
+    }
+
+});
